d30228e528abdae16289ba188d51c2f26e642f7f
OthmanYahya <othman.yahya996@gmail.com>
2022-08-04 22:49:20 +0300

    creation of form, todo list, completed and delete btns

---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Todo from "./Todo.js"
+
+
+const TodoList = ({ todos, setTodos }) => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {
+                    todos.map(
+                        (todo) => (
+                            <Todo
+                                todos={todos}
+                                setTodos={setTodos}
+                                todo={todo}
+                                key={todo.id}
+                                text={todo.text}
+                            />
+                        )
+                    )
+                }
+            </ul>
+
+        </div>
+    )
+}
+
+export default TodoList;
